551fda30f01d43935c522c3f79f8bbb260e230bd
Dmitro-sk <copicdmitro6@gmail.com>
2025-04-25 09:24:38 +0300

    Update pr2.py

--- pr2.py
+++ pr2.py
@@ -15,12 +15,13 @@ class Dmytro:
 
     def get_name_list(self):
 
-        return [self.first_name, self.last_name]
+        return [self.first_name, self.last_name] #повертає список
+        
 
 
 def _generate_student_id():
     import random
-    return f"ID-{random.randint(1000, 9999)}"
+    return f"ID-{random.randint(1000, 9999)}" #генерує ід студента 
 
 
 class ExtendedDmytro(Dmytro):
@@ -29,7 +30,7 @@ class ExtendedDmytro(Dmytro):
         self.university = university
         self.faculty = faculty
         self.specialty = specialty
-        self._student_id = _generate_student_id()
+        self._student_id = _generate_student_id() 
 
     def get_full_info(self):
         return {
@@ -39,12 +40,12 @@ class ExtendedDmytro(Dmytro):
             "university": self.university,
             "faculty": self.faculty,
             "specialty": self.specialty,
-            "student_id": self._student_id,
+            "student_id": self._student_id, #повна інформація про студента 
         }
 
     def is_graduated(self):
         course = self.calculate_course()
-        return course is not None and course >= 4
+        return course is not None and course >= 4 #перевіряє чи студент навчається 
 
 
 student = ExtendedDmytro("Дмитро", "Цьопич", 2008, "ЛНТУ", "ТФК", "Програмування")
